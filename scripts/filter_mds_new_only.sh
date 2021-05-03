59eccf7402d3682ceff9eb1ba68261933c9110ac
Kirstie Whitaker <kw401@cam.ac.uk>
2021-05-03 22:55:35 +0100

    filter files first

--- scripts/filter_mds_new_only.sh
+++ scripts/filter_mds_new_only.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# cd docs
-
+# Find the files that have changed since the last commit
 export DIFF=$( git diff --name-only HEAD~1 HEAD )
 #export DIFF=$( git diff --name-only ${{ github.event.before }} $GITHUB_SHA )
 echo ${DIFF[@]}
+
+# Make a temporary conversion directory
+mkdir -p tmp_mds_to_convert
+
+# Move the markdown files in DIFF to the new directory
+for f in ${DIFF[@]}; do
+  if [[ ${f} == *.md ]]; then
+    basename_f=`basename ${f}`
+    cp ${f} tmp_mds_to_convert/${basename_f}
+  fi
+done
