dfc958817e7e29cec9f712a80581c6bf954717dc
Kirstie Whitaker <kw401@cam.ac.uk>
2021-05-03 22:36:10 +0100

    update script

--- scripts/filter_pdfs_new_only.sh
+++ scripts/filter_pdfs_new_only.sh
@@ -2,6 +2,7 @@
 
 cd pdfs
 
-git fetch origin ${{ github.event.before }} --depth=1
-export DIFF=$( git diff --name-only ${{ github.event.before }} $GITHUB_SHA )
-echo "Diff between ${{ github.event.before }} and $GITHUB_SHA"
+git fetch origin --depth=1
+export DIFF=$( git diff --name-only HEAD~10 HEAD~5 )
+#export DIFF=$( git diff --name-only ${{ github.event.before }} $GITHUB_SHA )
+echo ${DIFF[@]}
